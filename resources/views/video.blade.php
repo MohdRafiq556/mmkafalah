5de4acb3a6fc9dfa4ebcaecd2677f8111595dd36
MohdRafiq556 <66372334+MohdRafiq556@users.noreply.github.com>
2022-01-25 21:05:11 +0800

    update youtube

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -16,91 +16,96 @@
                 <h4 style="padding:10px 0;">Bual Bicara Tentang Perancang Harta</h4>
             </div>
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=cNd3IZG6pU8&t=5s" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cNd3IZG6pU8" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=T-SggbAwJpM" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/T-SggbAwJpM" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=-QrcEJ4XidI" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-QrcEJ4XidI" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=5WNNOlz_h-o" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5WNNOlz_h-o" allowfullscreen></iframe>
             </div>
 
             <div class="p-3"></div>
             <div class="bg-white text-dark text-center p-2 border border-4 border-info">
                 <h4 style="padding:10px 0;">Testimoni Pelanggan</h4>
             </div>
+
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=5IQIdBbqNVE&t=1s" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5IQIdBbqNVE" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=XarGpQ3wq8o" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XarGpQ3wq8o" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=pvw5Ry3zRAA" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/pvw5Ry3zRAA" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=DYQ9Ntpnx7U" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/DYQ9Ntpnx7U" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=p2avEDrYrzM" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/p2avEDrYrzM" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=lAyrpEjMuII" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/lAyrpEjMuII" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=pgZ3EacmCuc" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/pgZ3EacmCuc" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=XSSSS0m9Y_4" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XSSSS0m9Y_4" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=F5VqpJuy_b0" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/F5VqpJuy_b0" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=AvmYv9mOzOg" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AvmYv9mOzOg" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=peccj9QW8Go" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/peccj9QW8Go" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=9leNCtvFH58" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/9leNCtvFH58" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=bKakM_uxXjI" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/bKakM_uxXjI" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=40leYyrPK0E" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/40leYyrPK0E" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=Q94NzY_Iwqk" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Av70RMcqEU4" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=GHsl9JQjWNw" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Q94NzY_Iwqk" allowfullscreen></iframe>
             </div>
 
             <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/watch?v=CiApDNiIWFQ" allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GHsl9JQjWNw" allowfullscreen></iframe>
+            </div>
+
+            <div class="col-lg-4 p-5 embed-responsive embed-responsive-1by1">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CiApDNiIWFQ" allowfullscreen></iframe>
             </div>
         </div>
     </div>
